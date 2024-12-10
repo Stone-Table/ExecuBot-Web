05efbbb2905673520fbe907382f2a98db4a14f79
teelrabbit <xuntitled@protonmail.com>
2024-12-09 17:02:54 -0800

    removing Your complete All-in-One solution for building SaaS services.

--- apps/nextjs/src/components/textGenerateEffect.tsx
+++ apps/nextjs/src/components/textGenerateEffect.tsx
@@ -2,7 +2,7 @@
 
 import { TextGenerateEffect } from "@saasfly/ui/text-generate-effect";
 
-const words = `Your complete All-in-One solution for building SaaS services. From coding to product launch, we have
+const words = ` From coding to product launch, we have
             everything you need covered!`;
 
 const TextGenerateEffects = () => {
