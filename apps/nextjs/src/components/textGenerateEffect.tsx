9e4e645c4fd4caecf5b26532c460085c202a4ef6
FBueller <55339121+teelrabbit@users.noreply.github.com>
2024-12-09 17:15:44 -0800

    Update textGenerateEffect.tsx

--- apps/nextjs/src/components/textGenerateEffect.tsx
+++ apps/nextjs/src/components/textGenerateEffect.tsx
@@ -2,8 +2,7 @@
 
 import { TextGenerateEffect } from "@saasfly/ui/text-generate-effect";
 
-const words = ` From coding to product launch, we have
-            everything you need covered!`;
+const words = ``;
 
 const TextGenerateEffects = () => {
   return <TextGenerateEffect words={words} />;
