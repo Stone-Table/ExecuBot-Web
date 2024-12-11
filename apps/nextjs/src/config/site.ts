92866cb1c831ebdbaf3817426c227171dbb3d071
teelrabbit <xuntitled@protonmail.com>
2024-12-11 11:33:59 -0800

    Updating Title Tags for SEO- ExecefficiencyAI 68 chars long

--- apps/nextjs/src/config/site.ts
+++ apps/nextjs/src/config/site.ts
@@ -1,5 +1,5 @@
 export const siteConfig = {
-  name: "Execubot",
+  name: 'ExecefficiencyAI - "Stanford says 10% of developers do *nothing*?"',
   description: "10% of developers do *nothing*? Execubot is gathering data from across the platforms engineers use. No more meetings–– Execubot is getting in the details now.",
   url: "https://github.com/saasfly/saasfly",
   ogImage: "",
