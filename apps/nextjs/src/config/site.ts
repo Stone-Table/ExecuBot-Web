83af9fc9f9a5253659daa023e26cdfef79ea0c75
teelrabbit <xuntitled@protonmail.com>
2024-12-11 10:03:14 -0800

    changing meta descriptions

--- apps/nextjs/src/config/site.ts
+++ apps/nextjs/src/config/site.ts
@@ -1,6 +1,6 @@
 export const siteConfig = {
   name: "Execubot",
-  description: "We provide an easier way to build saas service in production",
+  description: "10% of developers do *nothing*? Execubot is gathering data from across the platforms engineers use. No more meetings–– Execubot is getting in the details now.",
   url: "https://github.com/saasfly/saasfly",
   ogImage: "",
   links: {
