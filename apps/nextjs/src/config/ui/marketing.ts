2dd2ee1b4a877227f07b55ed18a231f60509c9c5
teelrabbit <xuntitled@protonmail.com>
2025-02-26 16:51:48 -0800

    adding chat button

--- apps/nextjs/src/config/ui/marketing.ts
+++ apps/nextjs/src/config/ui/marketing.ts
@@ -23,6 +23,10 @@ export const getMarketingConfig = async ({
         title: "Blog",
         href: "/blog",
       },
+      {
+        title: "Chat",
+        href: "/en/chat",
+      },
       // {
       //   title: "Documentation",
       //   href: "/docs",
