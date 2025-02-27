7791f8b32f0c641fa6929c52e2206cf017d4a6a2
teelrabbit <xuntitled@protonmail.com>
2025-02-26 16:37:04 -0800

    Update Clerk authentication configuration and middleware

--- apps/nextjs/src/middleware.ts
+++ apps/nextjs/src/middleware.ts
@@ -1,34 +1,10 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-import type { NextRequest } from "next/server";
-import { match as matchLocale } from "@formatjs/intl-localematcher";
-import Negotiator from "negotiator";
-import { i18n } from "~/config/i18n-config";
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-function getLocale(request: NextRequest): string {
-  const negotiatorHeaders: Record<string, string> = {};
-  request.headers.forEach((value, key) => (negotiatorHeaders[key] = value));
-  const languages = new Negotiator({ headers: negotiatorHeaders }).languages();
-  return matchLocale(languages, i18n.locales, i18n.defaultLocale);
-}
-
-// Define public routes that don't require authentication
-const isPublicRoute = createRouteMatcher([
-  "/",
-  "/sign-in(.*)",
-  "/sign-up(.*)",
-  "/api/webhooks(.*)",
-  "/api/stripe(.*)",
-  "/api/uploadthing"
-]);
-
-export default clerkMiddleware((auth, request) => {
-  if (!isPublicRoute(request)) {
-    auth.protect();
-  }
-});
+export default clerkMiddleware();
 
 export const config = {
-  // Protects all routes, including api/trpc
-  // See https://clerk.com/docs/references/nextjs/auth-middleware#matcher for more information about the matcher config
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: [
+    "/((?!.+\\.[\\w]+$|_next|_vercel|[^?]*\\.[^?]*$).*)",
+    "/"
+  ],
 };
